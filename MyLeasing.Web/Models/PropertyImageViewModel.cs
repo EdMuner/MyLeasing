2ea1b1beca4f819805ad3fb4e9db4d44b1c0588e
EdisonMunera <edisonmunera72@gmail.com>
2019-08-19 21:29:02 -0500

    Add image properti

--- MyLeasing.Web/Models/PropertyImageViewModel.cs
+++ MyLeasing.Web/Models/PropertyImageViewModel.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using MyLeasing.Web.Data.Entities;
 
 namespace MyLeasing.Web.Models
 {
-    public class PropertyImageViewModel
+    public class PropertyImageViewModel : PropertyImage
     {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
     }
 }
