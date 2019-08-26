b5861a2d3426c321d9499db9a4b10de9d867ecb1
EdisonMunera <edisonmunera72@gmail.com>
2019-08-25 20:46:42 -0500

    Add patial views

--- MyLeasing.Web/Controllers/AccountController.cs
+++ MyLeasing.Web/Controllers/AccountController.cs
@@ -52,5 +52,8 @@ namespace MyLeasing.Web.Controllers
             await _userHelper.LogoutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+
+    
     }
 }
--- MyLeasing.Web/Controllers/OwnersController.cs
+++ MyLeasing.Web/Controllers/OwnersController.cs
@@ -251,6 +251,8 @@ namespace MyLeasing.Web.Controllers
                 await _dataContext.SaveChangesAsync();
                 return RedirectToAction($"Details/{model.OwnerId}");
             }
+
+            model.PropertyTypes = _combosHelper.GetComboPropertyTypes();
             return View(model);
         }
 
@@ -400,6 +402,7 @@ namespace MyLeasing.Web.Controllers
                 return RedirectToAction($"{nameof(DetailsProperty)}/{model.PropertyId}");
             }
 
+            model.Lessees = _combosHelper.GetComboLessees();
             return View(model);
         }
 
