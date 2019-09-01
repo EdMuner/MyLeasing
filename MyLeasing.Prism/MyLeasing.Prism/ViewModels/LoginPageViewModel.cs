e6a88dc91f95a8a84a8f01bcf09cbcaa251e8161
EdisonMunera <edisonmunera72@gmail.com>
2019-09-01 15:47:03 -0500

    Modify Connection

--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/LoginPageViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/LoginPageViewModel.cs
@@ -1,29 +1,34 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
+﻿using MyLeasing.Common.Models;
+using MyLeasing.Common.Services;
+using Prism.Commands;
 using Prism.Navigation;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace MyLeasing.Prism.ViewModels
 {
     public class LoginPageViewModel : ViewModelBase
     {
+        public readonly IApiService _apiService;
         private String _password;
         private bool _isRunning;
         private bool _isEnabled;
         private DelegateCommand _loginCommand;
 
-        public LoginPageViewModel(INavigationService navigationservice) : base(navigationservice)
+        public LoginPageViewModel(
+            INavigationService navigationservice,
+            IApiService apiService) : base(navigationservice)
         {
             Title = "Login";
             IsEnabled = true;
-
+            _apiService = apiService;
+            //TODO: deleate this lines
+            Email = "jzuluaga55@hotmail.com";
+            Password = "123456";
         }
 
         public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand = new DelegateCommand(Login));
 
-       
+
 
         public String Email { get; set; }
 
@@ -42,6 +47,8 @@ namespace MyLeasing.Prism.ViewModels
             get => _isEnabled;
             set => SetProperty(ref _isEnabled, value);
         }
+        public IApiService ApiService { get; }
+
         private async void Login()
         {
             if (string.IsNullOrEmpty(Email))
@@ -55,6 +62,34 @@ namespace MyLeasing.Prism.ViewModels
                 await App.Current.MainPage.DisplayAlert("Error", "You must enter a password...", "Accept");
                 return;
             }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            var request = new TokenRequest
+            {
+                Password = Password,
+                Username = Email
+
+            };
+
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var response = await _apiService.GetTokenAsync(url, "Account", "/CreateToken", request);
+
+
+            IsRunning = false;
+            IsEnabled = true;
+
+            if (!response.IsSuccess)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await App.Current.MainPage.DisplayAlert("Error", "User or password incorrect..", "Accept");
+                Password = string.Empty;
+                return;
+            }
+
+            var token = response.Result;
             await App.Current.MainPage.DisplayAlert("Ok", "Fuck yeah!!...", "Accept");
 
         }
